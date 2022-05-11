78e37346106d1de2760f9844d4d862db9cb7fa81
tnmtvv <62231627+tnmtvv@users.noreply.github.com>
2022-05-11 15:57:57 +0300

    fixes + reading office

--- Trajectory.py
+++ Trajectory.py
@@ -1,40 +1,134 @@
+import sys
 from typing import List
 
 import numpy as np
 import math
-from config import CAMERA_ICL
+from config import CAMERA_ICL, PM, MAP_IP
 import cv2
+import read_office
+import evaluate_ate
+import evaluate_rpe
 from plane_extraction import *
 import open3d as o3d
 import os
+import subprocess as sp
 import argparse
 from mrob.mrob import FGraph, geometry, registration, LM
 
 
-def image_processing(path_color, path_depth, camera_intrinsics):
-    matrix_color = cv2.imread(path_color, cv2.IMREAD_COLOR)
-    matrix_depth = cv2.imread(path_depth, cv2.IMREAD_ANYDEPTH)
+def rotation_matrix_to_quaternion(r_matrix):
+    # First row of the rotation matrix
+    r00 = r_matrix[0, 0]
+    r01 = r_matrix[0, 1]
+    r02 = r_matrix[0, 2]
 
-    rows, columns, _ = matrix_color.shape
-    columns_indices = np.arange(columns)
+    # Second row of the rotation matrix
+    r10 = r_matrix[1, 0]
+    r11 = r_matrix[1, 1]
+    r12 = r_matrix[1, 2]
 
-    matrix_v = np.tile(columns_indices, (rows, 1))
-    matrix_u = np.transpose(np.tile(np.arange(rows), (columns, 1)))
+    # Third row of the rotation matrix
+    r20 = r_matrix[2, 0]
+    r21 = r_matrix[2, 1]
+    r22 = r_matrix[2, 2]
 
-    matrix_xyz = convert_from_plane_to_3d(
-        matrix_u,
-        matrix_v,
-        matrix_depth,
-        camera_intrinsics
-    )  # getting xyz coordinates of each point
+    tr = r00 + r11 + r22
 
-    matrix_of_points = matrix_xyz.reshape(-1, matrix_xyz.shape[2])  # now we have a list of points
-    reshaped_color_matrix = matrix_color.reshape(-1, matrix_color.shape[2])
-    return matrix_of_points, reshaped_color_matrix
+    if tr > 0:
+        s = math.sqrt(tr+1.0) * 2
+        qw = 0.25 * s
+        qx = (r21 - r12) / s
+        qy = (r02 - r20) / s
+        qz = (r10 - r01) / s
+    elif r00 > r11 & r00 > r22:
+        s = math.sqrt(1.0 + r00 - r11 - r22) * 2
+        qw = (r21 - r12) / s
+        qx = 0.25 * s
+        qy = (r01 + r10) / s
+        qz = (r02 + r20) / s
+
+    elif r11 > r22:
+        s = math.sqrt(1.0 + r11 - r00 - r22) * 2
+        qw = (r02 - r20) / s
+        qx = (r01 + r10) / s
+        qy = 0.25 * s
+        qz = (r12 + r21) / s
+    else:
+        s = math.sqrt(1.0 + r22 - r00 - r11) * 2
+        qw = (r10 - r01) / s
+        qx = (r02 + r20) / s
+        qy = (r12 + r21) / s
+        qz = 0.25 * s
+
+    q = [qx, qy, qz, qw]
+
+    return q
+
+
+def image_processing(function, depth_annot, camera_intrinsics, planes_matcher, map_indx_points, planes, first_or_sec):
+    points_of_images = []
+    colors_of_images = []
+
+    matrix_v = None
+
+    for image, depth in depth_annot:
+        matrix_color = cv2.imread(image, cv2.IMREAD_COLOR)
+        matrix_depth = cv2.imread(depth, cv2.IMREAD_ANYDEPTH)
+        print(image)
+        if matrix_v is None:
+            rows, columns, _ = matrix_color.shape
+            columns_indices = np.arange(columns)
+            matrix_v = np.tile(columns_indices, (rows, 1))
+            matrix_u = np.transpose(np.tile(np.arange(rows), (columns, 1)))
+        matrix_xyz = convert_from_plane_to_3d(
+            matrix_u,
+            matrix_v,
+            matrix_depth,
+            camera_intrinsics
+        )  # getting xyz coordinates of each point
+
+        points_of_image = matrix_xyz.reshape(-1, matrix_xyz.shape[2])  # now we have a list of points
+        colors_of_image = matrix_color.reshape(-1, matrix_color.shape[2])
+
+        points_of_images.append(points_of_image)
+        colors_of_images.append(colors_of_image)
+
+        if first_or_sec == 1:
+            function(points_of_image, colors_of_image, planes_matcher, map_indx_points)
+        elif first_or_sec == 2:
+            function(points_of_image, colors_of_image, planes_matcher, map_indx_points, planes)
+
+    return points_of_images, colors_of_images
+
+
+def image_processing_office(function, colors_files, depths_files, camera_intrinsics, planes_matcher,
+                            map_indx_points,
+                            planes,
+                            first_or_sec):
+
+        points_of_images = []
+        colors_of_images = []
+
+        for i, image in enumerate(colors_files):
+
+            colors_of_image = cv2.imread(image, cv2.IMREAD_COLOR)
+            colors_of_image = colors_of_image.reshape(-1, colors_of_image.shape[2])
+            colors_of_images.append(colors_of_image)
+
+            points_of_image = read_office.getting_points(i , depths_files, camera_intrinsics)
+            points_of_images.append(points_of_image)
+            print(image)
+
+            if first_or_sec == 1:
+                function(points_of_image, colors_of_image, planes_matcher, map_indx_points)
+            elif first_or_sec == 2:
+                function(points_of_image, colors_of_image, planes_matcher, map_indx_points, planes)
+
+        return points_of_images, colors_of_images
 
 
 def read_gt_poses(path, dataset_nodes):
-    in_file = open(path).read().splitlines()  # getting gt data
+    in_file = open(path).readlines()  # getting gt data
 
     all_lines = filter(lambda line: (line != ''), in_file)
 
@@ -43,44 +137,49 @@ def read_gt_poses(path, dataset_nodes):
     return gt_matrices
 
 
-def measure_error(num_of_nodes, graph_estimated_state, dataset_nodes):
-
-    gt_matrices = read_gt_poses('gt.txt', dataset_nodes)
-    last_gt_matrix = gt_matrices[num_of_nodes - 1]  # measuring error only by the last matrix
-    gt_matrix_01 = gt_matrices[0]
-
-    matrix_gt_the_last = np.append(last_gt_matrix, [[0, 0, 0, 1]], axis=0)
-    matrix_gt_first = np.append(gt_matrix_01, [[0, 0, 0, 1]], axis=0)
-
-    the_last_estimated_node = geometry.SE3(graph_estimated_state[-1])
-
-    absolute_to_the_last_gt = np.linalg.inv(matrix_gt_first) @ matrix_gt_the_last
-
-    matrix_gt_se3 = geometry.SE3(absolute_to_the_last_gt)
-
-    error_rotation = the_last_estimated_node.distance_rotation(matrix_gt_se3)
-    error_translation = the_last_estimated_node.distance_trans(matrix_gt_se3)
-
-    return error_rotation, error_translation
+def make_a_string(timestamp, translation, rotation):
+    translation_str = ' '.join(str(e) for e in translation)
+    rotation_str = ' '.join(str(e) for e in rotation)
+    return str(timestamp) + ' ' + translation_str + ' ' + rotation_str
 
 
-def estimate_the_graph(graph, num_of_nodes, planes, plane_index_to_real_index):
+def measure_error(first_node, first_gt_node, num_of_nodes, graph_estimated_state, ds_filename_gt, file_name_estimated,
+                  file_name_gt):
+
+    file_to_write_gt = open("measure_error_gt.txt", 'w')
+    file_to_read_gt = open(ds_filename_gt)
+
+    bios = 0
+    bios_gt = 0
+
+    in_file = file_to_read_gt.readlines()  # mind the first timestamp
+
+    if first_node == 0 and first_gt_node > 0:
+        bios = first_gt_node
+    elif first_gt_node > 0:
+        bios_gt = first_gt_node
+
+    for line in in_file[first_node - bios_gt:first_node + num_of_nodes - bios_gt]:
+        file_to_write_gt.write(line + '\n')
+    file_to_write_gt.close()
+
+    file_to_write_estimated = open(file_name_estimated, 'w')
+
+    estimated_matrices = graph_estimated_state[-num_of_nodes+bios:]
+    for i, matrix in enumerate(estimated_matrices):
+        data = make_a_string(first_node + i + bios, matrix[:3, 3], rotation_matrix_to_quaternion(matrix[:3, :3]))
+        file_to_write_estimated.write(data + '\n')
+    file_to_write_estimated.close()
+
+    print("ate")
+    evaluate_ate.main(file_name_gt, file_name_estimated)
+    print("rpe")
+    evaluate_rpe.main(file_name_gt, file_name_estimated)
+
+
+def estimate_the_graph(graph, num_of_nodes, planes, plane_index_to_real_index, graph_trajectory):
 
     w_z = np.identity(4)    # weight matrix
-
-    graph_trajectory = []
-
-    for i in range(num_of_nodes):
-        next_node = graph.add_node_pose_3d(geometry.SE3())
-        graph_trajectory.append(next_node)
-    graph.add_factor_1pose_3d(geometry.SE3(), graph_trajectory[0], 1e6 * np.identity(6))
-
-    for i in range(num_of_nodes):
-        real_indx = len(plane_index_to_real_index)
-        for plane in planes[i]:
-            if plane.index not in plane_index_to_real_index:
-                plane_index_to_real_index[plane.index] = real_indx
-                real_indx += 1
 
     for n in range(num_of_nodes):
         for plane in planes[n]:
@@ -93,21 +192,31 @@ def estimate_the_graph(graph, num_of_nodes, planes, plane_index_to_real_index):
     return graph_estimated_state
 
 
-def build_the_graph(num_of_nodes, planes: List[List[Plane]]):
+def build_the_graph(num_of_nodes, planes: List[List[Plane]], plane_index_to_real_index):
     graph = FGraph()
-    set_indx = set()
 
     for i in range(num_of_nodes):
+        real_indx = len(plane_index_to_real_index)
         for plane in planes[i]:
-            if plane.index not in set_indx:
-                graph.add_node_plane_4d(
-                    np.array([1, 0, 0, 0]))
-                set_indx.add(plane.index)
+            if plane.index not in plane_index_to_real_index:
+                plane_index_to_real_index[plane.index] = real_indx
+                real_indx += 1
 
-    return graph
+    for i, _ in enumerate(plane_index_to_real_index):
+        graph.add_node_plane_4d(
+            np.array([1, 0, 0, 0]))
+
+    graph_trajectory = []
+
+    for i in range(num_of_nodes):
+        next_node = graph.add_node_pose_3d(geometry.SE3())
+        graph_trajectory.append(next_node)
+    graph.add_factor_1pose_3d(geometry.SE3(), graph_trajectory[0], 1e6 * np.identity(6))
+
+    return graph, graph_trajectory
 
 
-def visualisation(graph_estimaed_state, num_of_nodes, matrices_of_points, colors):
+def visualisation(graph_estimated_state, num_of_nodes, matrices_of_points, colors):
 
     point_clouds = []
 
@@ -125,7 +234,7 @@ def visualisation(graph_estimaed_state, num_of_nodes, matrices_of_points, colors
     )
 
     pc_answ = o3d.geometry.PointCloud()
-    map_cloud_matrix = {point_clouds: graph_estimaed_state for point_clouds, graph_estimaed_state in zip(reversed(point_clouds), reversed(graph_estimaed_state))}
+    map_cloud_matrix = {point_clouds: graph_estimated_state for point_clouds, graph_estimated_state in zip(reversed(point_clouds), reversed(graph_estimated_state))}
 
     for pc in point_clouds:
         pc_answ += pc.transform(map_cloud_matrix[pc]).transform(reflection)
@@ -134,66 +243,55 @@ def visualisation(graph_estimaed_state, num_of_nodes, matrices_of_points, colors
     o3d.visualization.draw_geometries([pc_answ])
 
 
-def main(path_depth, path_color, dataset_nodes):
+def main(path_color, path_orig_color, num_of_nodes, general_path_orig, general_path):
 
-    depths = sorted(os.listdir(path_depth), key=lambda x: int(x[:-4]))
-    pahlava = sorted(os.listdir(path_color), key=lambda x: int(x[:-4]))
+    pahlava = os.listdir(path_color)
+    pahlava = sorted(pahlava, key=read_office.__filenames_sorted_mapper)
+    full_pahlava = list(map(lambda x: os.path.join(path_color, x), pahlava))
 
-    num_of_nodes = len(depths)
+    pahlava_orig = os.listdir(path_orig_color)
+    pahlava_orig = sorted(pahlava_orig, key=read_office.__filenames_sorted_mapper)
+    full_pahlava_orig = map(lambda x: os.path.join(path_orig_color, x), pahlava_orig)
 
-    depth_annot =\
-        [[os.path.join(path_color, pahlava[i]), os.path.join(path_depth, depths[i])] for i, _ in enumerate(depths)]
+    png_files, depths_files = read_office.provide_filenames(general_path)
+    png_files_orig, depths_files_orig = read_office.provide_filenames(general_path_orig)
 
     planes = []
-    matrices_of_points = []
-    colors = []
 
-    map_indx_points = {}
-    planes_matcher = {}
+    planes_matcher = PM
+    map_indx_points = MAP_IP
 
-    for image, depth in depth_annot:
-        points_of_image, colors_of_image = image_processing(image, depth, CAMERA_ICL)
-        building_maps(points_of_image, colors_of_image, planes_matcher, map_indx_points)
+    if len(planes_matcher) == 0:
+        lambda_1 = lambda x, y, z, q: building_maps(x, y, z, q)
+        image_processing_office(lambda_1, full_pahlava_orig, depths_files_orig, CAMERA_ICL, planes_matcher, map_indx_points, planes=None, first_or_sec=1)
 
-    max_points_indx = {}
+    map_indx_points_sorted = sorted(map_indx_points, key=map_indx_points.get)
+    max_indices = map_indx_points_sorted[-4:]
 
-    for key, value in map_indx_points.items():
-        max_points_indx[value] = key
-
-    list_of_max_points = list(map_indx_points.values())
-    ten_max_points = sorted(list_of_max_points)[-10:]
-
-    ten_max_indices = []
-
-    for points in ten_max_points:
-        ten_max_indices.append(max_points_indx[points])
-
-    for image, depth in depth_annot:
-        points_of_image, colors_of_image = image_processing(image, depth, CAMERA_ICL)
-        planes_of_image = equation_extraction(points_of_image, colors_of_image, planes_matcher, ten_max_indices)
-
-        matrices_of_points.append(points_of_image)
-        colors.append(colors_of_image)
-        planes.append(planes_of_image)
+    lambda_2 = lambda x, y, z, q, p: equation_extraction(x, y, z, q, p)
+    matrices_of_points, colors = \
+        image_processing_office(lambda_2, full_pahlava, depths_files, CAMERA_ICL, planes_matcher, max_indices, planes, 2)
 
     plane_index_to_real_index = {}
 
-    graph = build_the_graph(num_of_nodes, planes)
-    graph_estimated_state = estimate_the_graph(graph, num_of_nodes, planes, plane_index_to_real_index)
+    graph, graph_trajectory = build_the_graph(num_of_nodes, planes, plane_index_to_real_index)
+    graph_estimated_state = estimate_the_graph(graph, num_of_nodes, planes, plane_index_to_real_index, graph_trajectory)
 
-    error_rotation, error_translation = measure_error(num_of_nodes, graph_estimated_state, dataset_nodes)
-    print(error_rotation, error_translation)
+    measure_error(204, 2, num_of_nodes, graph_estimated_state, 'quaternion_gt_office.txt', 'measure_error_estimated.txt',
+                  'measure_error_gt.txt')
 
     visualisation(graph_estimated_state, num_of_nodes, matrices_of_points, colors)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Build a trajectory')
-    parser.add_argument('path_depth', type=str, help='Directory, where depth images are stored')
     parser.add_argument('path_color', type=str, help='Directory where color images are stored')
+    parser.add_argument('path_orig_color', type=str, help='Directory where color images are stored')
     parser.add_argument('number_of_nodes', type=int, help='Number of nodes in the dataset')
+    parser.add_argument('general_path_orig', type=str, help='Directory, where all the files are stored')
+    parser.add_argument('general_path', type=str, help='Directory, where working files are stored')
 
     args = parser.parse_args()
 
-    main(args.path_depth, args.path_color, args.number_of_nodes)
+    main(args.path_color, args.path_orig_color, args.number_of_nodes, args.general_path_orig, args.general_path)
     
--- plane_extraction.py
+++ plane_extraction.py
@@ -5,7 +5,9 @@ import open3d as o3d
 
 
 class Camera(object):
-    def __init__(self, cx, cy, focal_x, focal_y, scale):
+    def __init__(self, width, height, cx, cy, focal_x, focal_y, scale):
+        self.width = width
+        self.height = height
         self.cx = cx
         self.cy = cy
         self.focal_x = focal_x
@@ -28,15 +30,20 @@ def array_to_string(array: np.array) -> str:
 
 
 def convert_from_plane_to_3d(u, v, depth, cam: Camera):
-    x_over_z = (v - cam.cx) / cam.focal_x
+    #result = np.zeros((u.shape, 3))
+    x_over_z = (v - cam.cx) / cam.focal_x # создать матрицу result (rows, colums, 3)
     y_over_z = (u - cam.cy) / cam.focal_y
 
+    # result[::2] = depth / cam.scale
+    # result[::1] = y_over_z * result[::2]
+    # result[::0] = x_over_z * result[::2]
     z_matrix = depth / cam.scale
 
     x_matrix = x_over_z * z_matrix
     y_matrix = y_over_z * z_matrix
 
     return np.dstack((x_matrix, y_matrix, z_matrix))
+
 
 
 def get_normal(points):
@@ -52,9 +59,9 @@ def get_normal(points):
     return np.asarray([normal[0], normal[1], normal[2], d])
 
 
-def building_maps(points, point_colors, color_to_index, indx_to_max_num_points):
+def building_maps(points, colors, color_to_index, indx_to_max_num_points):
 
-    colors_unique = np.unique(point_colors, axis=0)
+    colors_unique = np.unique(colors, axis=0)
 
     num_unique_colors, _ = colors_unique.shape
     num_of_points, _ = points.shape
@@ -68,7 +75,7 @@ def building_maps(points, point_colors, color_to_index, indx_to_max_num_points):
             color_to_index[color_string] = indx  # append (color:index) to map with the next index
             indx += 1
         cur_indx = color_to_index[color_string]
-        indices = np.where((point_colors == color).all(axis=1))
+        indices = np.where((colors == color).all(axis=1))
         if (cur_indx in indx_to_max_num_points and len(indices[0]) > indx_to_max_num_points[cur_indx]) \
                 or cur_indx not in indx_to_max_num_points:
             indx_to_max_num_points[cur_indx] = len(indices[0])
@@ -76,19 +83,25 @@ def building_maps(points, point_colors, color_to_index, indx_to_max_num_points):
     return color_to_index, indx_to_max_num_points
 
 
-def equation_extraction(points, point_colors, color_to_index, max_planes):
-    colors_unique = np.unique(point_colors, axis=0)
+def equation_extraction(points, colors, color_to_index, max_planes, planes):
+    colors_unique = np.unique(colors, axis=0)
 
-    planes = []
     unique_colors_without_black = list(filter(lambda x: (x != [0, 0, 0]).all(axis=0), colors_unique))
-
+    planes_of_image = []
+    cur_colors = np.zeros(points.shape)
     for color in unique_colors_without_black:
         color_string = array_to_string(color)
         cur_indx = color_to_index[color_string]
-        indices = np.where((point_colors == color).all(axis=1))
+        indices = np.where((colors == color).all(axis=1))
         if cur_indx in max_planes:
             plane_points = points[indices[0]]
+            cur_colors[indices] = colors[indices]
             plane = Plane(get_normal(plane_points), color_to_index[color_string])
-            planes.append(plane)
+            planes_of_image.append(plane)
+    planes.append(planes_of_image)
+    # pc = o3d.geometry.PointCloud()
+    # pc.points = o3d.utility.Vector3dVector(points)
+    # pc.colors = o3d.utility.Vector3dVector(cur_colors.astype(np.float64) / 255.0)
+    # o3d.visualization.draw_geometries([pc])
 
     return planes
